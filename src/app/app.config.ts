05e6ea028d75858de834a4b30be78b9596b6fb1e
Luca Knaack <l.knaack93@gmail.com>
2024-04-14 21:26:00 +0200

    redo config secret management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,24 +12,14 @@ import {
     UserTrackingService,
 } from '@angular/fire/analytics'
 import { getFirestore, provideFirestore } from '@angular/fire/firestore'
+import { environment } from '../environments/environment'
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
         provideClientHydration(),
         importProvidersFrom(
-            provideFirebaseApp(() =>
-                initializeApp({
-                    projectId: 'pa-bailar-fca75',
-                    appId: '1:366596063328:web:a2229d12d506af5d3e0305',
-                    storageBucket: 'pa-bailar-fca75.appspot.com',
-                    // locationId: 'europe-west',
-                    apiKey: 'AIzaSyDnyeuIYc9CpC39Vu6j3D5KGhEVAbF-oPc',
-                    authDomain: 'pa-bailar-fca75.firebaseapp.com',
-                    messagingSenderId: '366596063328',
-                    measurementId: 'G-4PMBXNC6F1',
-                })
-            )
+            provideFirebaseApp(() => initializeApp(environment.firebaseConfig))
         ),
         importProvidersFrom(provideAuth(() => getAuth())),
         importProvidersFrom(provideAnalytics(() => getAnalytics())),
