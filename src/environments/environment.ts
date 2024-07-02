b3b0c9070541772447b8abdf5a3ad6047a42d57b
Luca Knaack <l.knaack93@gmail.com>
2024-07-02 23:23:18 +0200

    add environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyDnyeuIYc9CpC39Vu6j3D5KGhEVAbF-oPc",
+  
+    authDomain: "pa-bailar-fca75.firebaseapp.com",
+  
+    projectId: "pa-bailar-fca75",
+  
+    storageBucket: "pa-bailar-fca75.appspot.com",
+  
+    messagingSenderId: "366596063328",
+  
+    appId: "1:366596063328:web:7d15603f8fc293713e0305",
+  
+    measurementId: "G-RD8PGD4SWX"
+  
+  };
+  
